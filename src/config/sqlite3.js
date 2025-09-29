9cee68afae5b77fbedb50b3dc893a0285a87aa11
Dylan Sanders <dsand171@mtroyal.ca>
2025-09-29 13:30:22 -0600

    DB connection/close module written and functional.

---
+++ src/config/sqlite3.js
@@ -0,0 +1,24 @@
+// Import sqlite3 module.
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
+
+const dbPath = path.join(__dirname, "../data/f1.db");
+
+const db = new sqlite3.Database(dbPath, (err) => {
+    if (err) {
+        console.log('Database connection failed: ' + err.message);
+    } else {
+        console.log('Database connection successful!');
+    }
+});
+
+
+function dbClose() {
+    db.close();
+    console.log('Database connection closed!');
+}
+
+db.close();
+
+
+module.exports = {db, dbClose};
